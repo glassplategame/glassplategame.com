5ac33b8b5d44a13f68bd4f72f900e2630c00d518
spectralsun <garrett@spectralsun.com>
2014-05-11 23:45:03 -0700

    assets: add jquery and bootstrap

--- gpgcom/assets.py
+++ gpgcom/assets.py
@@ -1,9 +1,16 @@
 from flask.ext.assets import Bundle
 
+from flask_spirits.assets import js_jquery, js_bootstrap, css_bootstrap
 
-js_public = Bundle()
+js_public = Bundle(
+    js_jquery,
+    js_bootstrap
+)
 
-css_public = Bundle()
+css_public = Bundle(
+    css_bootstrap,
+    'css/gpg.css'
+)
 
 js_admin = Bundle()
 
