4ff8cb2cb6533a68dcd21922307f560d1cb6b829
spectralsun <garrett@spectralsun.com>
2014-05-15 23:53:03 -0700

    assets: added gpg, alertify, davis and lodash

--- gpgcom/assets.py
+++ gpgcom/assets.py
@@ -1,14 +1,20 @@
 from flask.ext.assets import Bundle
 
-from flask_spirits.assets import js_jquery, js_bootstrap, css_bootstrap
+from flask_spirits.assets import js_jquery, js_bootstrap, css_bootstrap, \
+    js_davis, js_lodash, js_alertify, css_alertify
 
 js_public = Bundle(
     js_jquery,
-    js_bootstrap
+    js_bootstrap,
+    js_alertify,
+    js_davis,
+    js_lodash,
+    'js/gpg.js'
 )
 
 css_public = Bundle(
     css_bootstrap,
+    css_alertify,
     'css/gpg.css'
 )
 
