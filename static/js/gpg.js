5f60626d22e2bd976e54486449d589facff1e1fd
spectralsun <garrett@spectralsun.com>
2014-06-03 22:39:33 -0700

    gpg.js: clean up and add set_page

--- static/js/gpg.js
+++ static/js/gpg.js
@@ -1,17 +1,23 @@
-;(function() {
-    var $pages = $('.page');
-    var pages = {};
-    var hashes = ['home', 'about', 'playing_the_game', 'current_playing', 'cards', 'contact', 'links'];
-    var all_pages = hashes;
-    var nav = $('.navbar li');
-    var nav_collapse = $('.navbar-collapse');
-    var error_page = $('#error');
-    var error = $('.error', error_page);
-    $.map(all_pages, function(hash) { pages[hash] = $('#' + hash); });
-     
+;(function($document, $page, $navbar_li) {
+    var $pages = {};
+    var pages = [
+        'home', 
+        'about', 
+        'hesse', 
+        'playing_the_game', 
+        'current_playing', 
+        'cards', 
+        'contact', 
+        'links'
+    ];
+    var all_pages = pages;
+    $.map(all_pages, function(id) { $pages[id] = $('#' + id); });
+    
+    // HTML5 Routing 
     var routing = Davis(function() {
         var self = this;
-        $.map(_.union([''], hashes), function(page) {
+        // Here we merge the pages array with a blank string to account for no path
+        $.map(_.union([''], pages), function(page) {
             self.get('/' + page, function() {
                 if (page == '')
                     page = 'home';
@@ -26,4 +32,12 @@
     routing.start();
     var noop = function() {};
     //routing.logger = { info: noop, warning: noop, error: noop };
-})();
+
+    function set_page(id) {
+        $page.hide();
+        $navbar_li.removeClass('active');
+        $pages[id].show();
+        $('a[href="/' + id + '"]', $navbar_li).parent().addClass('active');
+        $document.trigger('set-page-' + id)
+    }
+})($(document), $('.page'), $('.navbar li'));
