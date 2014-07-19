b5c5bd415b2ca48c744b6ca2782ac9629f2c72f7
spectralsun <garrett@spectralsun.com>
2014-07-18 17:37:13 -0700

    manage: add user commands

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 from flask.ext.script import Manager
+from flask_spirits.manage import user
 
 from gpgcom.main import app
 
 
 manager = Manager(app)
+
+manager.add_command('user', user.manager)
 
 if __name__ == '__main__':
     manager.run()
