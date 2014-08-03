577e5f1aca12183931d349ee3f76fa0bbe69739e
spectralsun <garrett@spectralsun.com>
2014-08-02 18:54:19 -0700

    assets: include js_forms, css_forms and css_modal from flask-spirits

--- gpgcom/assets.py
+++ gpgcom/assets.py
@@ -1,7 +1,8 @@
 from flask.ext.assets import Bundle
 
 from flask_spirits.assets import js_jquery, js_bootstrap, css_bootstrap, \
-    js_davis, js_lodash, js_alertify, css_alertify
+    js_davis, js_lodash, js_alertify, js_forms, css_alertify, css_modal, \
+    css_forms
 
 js_public = Bundle(
     js_jquery,
@@ -9,12 +10,16 @@ js_public = Bundle(
     js_alertify,
     js_davis,
     js_lodash,
+
+    js_forms,
     'js/gpg.js'
 )
 
 css_public = Bundle(
     css_bootstrap,
     css_alertify,
+    css_forms,
+    css_modal,
     'css/gpg.css'
 )
 
