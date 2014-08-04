dd7b4dda3f49e91a23e8f7fc31a8a9080a4cb92b
spectralsun <garrett@spectralsun.com>
2014-08-04 01:50:11 -0700

    assets: add datetimepicker

--- gpgcom/assets.py
+++ gpgcom/assets.py
@@ -2,11 +2,12 @@ from flask.ext.assets import Bundle
 
 from flask_spirits.assets import js_jquery, js_bootstrap, css_bootstrap, \
     js_davis, js_lodash, js_alertify, js_forms, css_alertify, css_modal, \
-    css_forms
+    css_forms, js_datetimepicker, css_datetimepicker
 
 js_public = Bundle(
     js_jquery,
     js_bootstrap,
+    js_datetimepicker,
     js_alertify,
     js_davis,
     js_lodash,
@@ -17,6 +18,7 @@ js_public = Bundle(
 
 css_public = Bundle(
     css_bootstrap,
+    css_datetimepicker,
     css_alertify,
     css_forms,
     css_modal,
