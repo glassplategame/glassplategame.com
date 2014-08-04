fc59e3adeea4c819120dd9e076c74fb27bed740b
spectralsun <garrett@spectralsun.com>
2014-08-04 01:56:26 -0700

    game model: change description to text

--- gpgcom/models.py
+++ gpgcom/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, DateTime, Integer, String, Boolean
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, Text
 
 from flask_spirits.database import Model
 
@@ -9,7 +9,7 @@ class Game(Model):
     id = Column(Integer, primary_key=True)
     title = Column(String, info={'label': 'Title'})
     theme = Column(String, info={'label': 'Theme'})
-    description = Column(String, info={'label': 'Description'})
+    description = Column(Text, info={'label': 'Description'})
     start = Column(DateTime, nullable=False, info={'label': 'Start'})
     end = Column(DateTime, info={'label': 'End (optional)'})
     location_one = Column(String, info={'label': 'Location One'})
