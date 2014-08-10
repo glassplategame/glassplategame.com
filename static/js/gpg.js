950fd9a3b23eb1155a9d6dcac5b001128c40c574
spectralsun <garrett@spectralsun.com>
2014-08-09 22:44:56 -0700

    gpg.js: csrf token

--- static/js/gpg.js
+++ static/js/gpg.js
@@ -58,4 +58,12 @@
         });
     })
 
+    var csrf = $('meta[name="csrf-token"]').attr('content');
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrf)
+            }
+        }
+    });
 })($(document), $('.page'), $('#menu li'));
