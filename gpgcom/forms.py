7a93675b29a3c5e0ce30475d2347df0a986ab01f
spectralsun <garrett@spectralsun.com>
2014-08-11 22:37:46 -0700

    forms: contact form

--- gpgcom/forms.py
+++ gpgcom/forms.py
@@ -1,4 +1,7 @@
-from wtforms.form import Form
+from flask_wtf import Form
+from wtforms import StringField 
+from wtforms.fields import TextAreaField
+from wtforms.validators import DataRequired
 from wtforms_alchemy import ModelForm
 
 from gpgcom.models import Game
@@ -8,3 +11,9 @@ class GameForm(ModelForm):
     class Meta:
         model = Game
         exclude = ['updated_at']
+
+class ContactForm(Form):
+    email = StringField('Email', validators=[DataRequired()])
+    name = StringField('Name')
+    subject = StringField('Subject')
+    message = TextAreaField('Message', validators=[DataRequired()])
